b6129755b954cd3f055294d6cc7f56d5a4649c83
michaeldcurry1 <michaeldcurry1@gmail.com>
2013-04-25 01:31:32 +0200

    Update and rename ba.java to Enemies.java

--- Nightmare/src/net/amigocraft/Nightmare/Enemies.java
+++ Nightmare/src/net/amigocraft/Nightmare/Enemies.java
@@ -1,5 +1,5 @@
 /** ENEMIES **/
-package net.amigocraft.Nightmare;
+package net.amigocraft.nightmare;
 
 import java.awt.Image;
 import java.awt.Point;
@@ -12,7 +12,7 @@ import java.util.List;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class ba extends JPanel {
+public class Enemies extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
@@ -37,14 +37,14 @@ public class ba extends JPanel {
 		enemyDim.clear();
 		enemyDim.put("skeleton", new int[]{26, 46});
 		try {
-			skeleton.add(ImageIO.read(ad.class.getResourceAsStream("/images/Skeleton1.png")));
-			skeleton.add(ImageIO.read(ad.class.getResourceAsStream("/images/Skeleton2.png")));
-			skeleton.add(ImageIO.read(ad.class.getResourceAsStream("/images/Skeleton3.png")));
-			skeleton.add(ImageIO.read(ad.class.getResourceAsStream("/images/Skeleton4.png")));
-			skeletonF.add(ImageIO.read(ad.class.getResourceAsStream("/images/SkeletonF1.png")));
-			skeletonF.add(ImageIO.read(ad.class.getResourceAsStream("/images/SkeletonF2.png")));
-			skeletonF.add(ImageIO.read(ad.class.getResourceAsStream("/images/SkeletonF3.png")));
-			skeletonF.add(ImageIO.read(ad.class.getResourceAsStream("/images/SkeletonF4.png")));
+			skeleton.add(ImageIO.read(Character.class.getResourceAsStream("/images/Skeleton1.png")));
+			skeleton.add(ImageIO.read(Character.class.getResourceAsStream("/images/Skeleton2.png")));
+			skeleton.add(ImageIO.read(Character.class.getResourceAsStream("/images/Skeleton3.png")));
+			skeleton.add(ImageIO.read(Character.class.getResourceAsStream("/images/Skeleton4.png")));
+			skeletonF.add(ImageIO.read(Character.class.getResourceAsStream("/images/SkeletonF1.png")));
+			skeletonF.add(ImageIO.read(Character.class.getResourceAsStream("/images/SkeletonF2.png")));
+			skeletonF.add(ImageIO.read(Character.class.getResourceAsStream("/images/SkeletonF3.png")));
+			skeletonF.add(ImageIO.read(Character.class.getResourceAsStream("/images/SkeletonF4.png")));
 			enemySprites.put("skeleton", skeleton);
 			enemySpritesF.put("skeleton", skeletonF);
 		} 
@@ -52,8 +52,8 @@ public class ba extends JPanel {
 			e.printStackTrace();
 		}
 		enemies.clear();
-		createEnemy(posRel(0, 1, 3), bb.floors.get(0).y, "skeleton", 1);
-		createEnemy(posRel(1, 1, 2), bb.floors.get(1).y, "skeleton", 1);
+		createEnemy(posRel(0, 1, 3), Platforms.floors.get(0).y, "skeleton", 1);
+		createEnemy(posRel(1, 1, 2), Platforms.floors.get(1).y, "skeleton", 1);
 	}
 	
 	public static void createEnemy(int x, int y, String type, int level){
@@ -130,13 +130,13 @@ public class ba extends JPanel {
 			}
 			
 			// animation
-			if (e.getAnimationFrame() >= ba.aniSpeed){
+			if (e.getAnimationFrame() >= Enemies.aniSpeed){
 				List<Image> loop = null;
 				if (e.getDirection() == 0)
 					loop = e.getSprites();
 				else
 					loop = e.getSpritesF();
-				if (ac.inGame){
+				if (MainCanvas.inGame){
 					if (e.getAnimationStage() < loop.size() - 1){
 						e.setAnimationStage(e.getAnimationStage() + 1);
 					}
@@ -148,22 +148,22 @@ public class ba extends JPanel {
 			else
 				e.setAnimationFrame(e.getAnimationFrame() + 1);
 
-			if (new Rectangle(e.getX(), e.getY(), e.getWidth(), e.getHeight()).intersects(ad.character)){
-				if (!ad.invincible){
-					ad.health -= 3;
-					ad.knockback = true;
-					if (ad.health > 0)
-						ad.invincible = true;
+			if (new Rectangle(e.getX(), e.getY(), e.getWidth(), e.getHeight()).intersects(Character.character)){
+				if (!Character.invincible){
+					Character.health -= 3;
+					Character.knockback = true;
+					if (Character.health > 0)
+						Character.invincible = true;
 					else
-						ad.dead = true;
+						Character.dead = true;
 				}
 			}
 		}
 	}
 
 	public static boolean containsFeet(Point f1){
-		for (int i = 0; i < bb.floors.size(); i++){
-			if (bb.floors.get(i).contains(f1)){
+		for (int i = 0; i < Platforms.floors.size(); i++){
+			if (Platforms.floors.get(i).contains(f1)){
 				return true;
 			}
 		}
@@ -171,6 +171,6 @@ public class ba extends JPanel {
 	}
 	
 	public static int posRel(int floorIndex, int num, int den){
-		return (int)(bb.floors.get(floorIndex).x + (bb.floors.get(floorIndex).width * (num / (double)den)));
+		return (int)(Platforms.floors.get(floorIndex).x + (Platforms.floors.get(floorIndex).width * (num / (double)den)));
 	}
 }
